db9b2a3eb8f361d1ff9f76006fd1ab5ab28214fb
john kyony <jkyony@gmail.com>
2023-09-18 13:57:50 +0200

    Created typeOrm entities User and Task

---
+++ src/entities/Task.ts
@@ -0,0 +1,18 @@
+import { Entity , PrimaryGeneratedColumn , Column , ManyToOne } from "typeorm";
+import {User} from "./User";
+
+@Entity()
+export class Task{
+    @PrimaryGeneratedColumn()
+    id:number;
+
+    @Column()
+    title: string;
+
+    @Column()
+    description: string;
+
+    @ManyToOne(() => User, (user) => user.tasks)
+    user: User;
+
+} 
---
+++ src/entities/User.ts
@@ -0,0 +1,16 @@
+import { Entity , PrimaryGeneratedColumn , Column  , OneToMany} from "typeorm";
+import {Task} from './Task';
+@Entity()
+export class User{
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    username: string;
+
+    @Column()
+    email: string;
+
+    @OneToMany(() => Task , (task) => task.user)
+    tasks: Task[];
+}
